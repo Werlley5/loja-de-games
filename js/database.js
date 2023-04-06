d316b7f17be309da768f684d88ef0847ebf68cd6
Werlley5 <110196227+Werlley5@users.noreply.github.com>
2023-04-06 17:13:38 -0300

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,62 @@
+const data = [
+    {
+      id: 1,
+      img: "./img/ps5-luxo.jpg",
+      nameItem: "PS5 luxo",
+      description:
+        "Console em ouro e controle com couro de crocodilo",
+      value:  "50.000,00",
+      add: "Comprar",
+      tag: "Consoles",
+    },
+    {
+      id: 2,
+      img: "./img/ps5.jpg",
+      nameItem: "PS5",
+      description:
+        "PS5 padrão com custo benefício melhor",
+      value: "4.500,00",
+      add: "Comprar",
+      tag: "Consoles",
+    },
+    {
+      id: 3,
+      img: "./img/god_of_war_ragnarok.jpg",
+      nameItem: "God of War Ragnarok",
+      description:
+        " Kratos e Atreus viajarão para cada canto dos...",
+      value: "349,89",
+      add: "Comprar",
+      tag: "Jogos",
+    },
+    {
+      id: 4,
+      img: "./img/horizon_forbidden_west.png",
+      nameItem: "Horizon Forbidden West",
+      description:
+        "Junte-se à Aloy para desbravar o Oeste Proibido...",
+      value: "184,99",
+      add: "Comprar",
+      tag: "Jogos",
+    },
+    {
+      id: 5,
+      img: "./img/acessorio.jpg",
+      nameItem: "Etação de carregamento",
+      description:
+        "Estação de Carregamento Sem Fio é projetada...",
+      value:  "179,50",
+      add: "Comprar",
+      tag: "Acessórios",
+    },
+    {
+      id: 6,
+      img: "./img/acessorio2.jpeg",
+      nameItem: "Suporte de parede",
+      description:
+        "suporte de parede suporte de rack suporte de rack",
+      value: "199.90",
+      add: "Comprar",
+      tag: "Acessórios",
+    },
+  ];
